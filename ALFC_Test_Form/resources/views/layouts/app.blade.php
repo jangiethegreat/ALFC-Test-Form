4427ad80aaa4a79dbe2288bd4dcd44b0c5905384
jangiethegreat <fernandojohncris@gmail.com>
2023-11-23 14:01:56 +0800

    data table design

--- ALFC_Test_Form/resources/views/layouts/app.blade.php
+++ ALFC_Test_Form/resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <title>Your Laravel App</title>
     <!-- Include Bootstrap CSS from a CDN -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
@@ -79,5 +82,8 @@
 
     <!-- Include Bootstrap JS from a CDN -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+     <!-- DataTables JS -->
+     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 </body>
 </html>
