75ff030625e9396cad929cc200d8c2b12766b531
jangiethegreat <fernandojohncris@gmail.com>
2023-11-24 11:08:02 +0800

    fgen-draft for category mobile view responsive

--- ALFC_Test_Form/resources/views/users/fgencategory.blade.php
+++ ALFC_Test_Form/resources/views/users/fgencategory.blade.php
@@ -2,60 +2,145 @@
 
 @section('content')
 
+<style>
+    .card {
+        border-radius: 10px;
+        background: #CC2E2E;
+        width: 200px;
+        height: 125px; /* Adjusted height */
+        flex-shrink: 0;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-size: auto;
+        margin:5px;
+    }
 
+    /* Override Bootstrap's default column padding */
+ 
+    
+    .cardView .card {
+        width: 100px; /* Set your preferred width */
+        height: 65px; /* Set your preferred height */
+        background-size: 70px 45px;
+
+    }
+    .cardView .card:hover {
+        transform: scale(1.1); /* Increases the size of the card on hover */
+        transition: transform 0.3s ease; /* Adds a smooth transition effect */
+    }
+    .card:hover {
+        transform: scale(1.1); /* Increases the size of the card on hover */
+        transition: transform 0.3s ease; /* Adds a smooth transition effect */
+    }
+    
+    
+</style>
 
 <h2 style="text-align: center; color: #332727; font-family: Montserrat; font-size: 40px; font-weight: 700; margin-top: 45px; margin-bottom: 50px;">
     Insurances for FGEN
 </h2>
-<div class = "container">
-    <div class="row justify-content-center mt-4">
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/fgen.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto;  "></div>
+<div class = "container-fluid d-md-block d-none">
+    <div class="row justify-content-center">
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/apa.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/kemp.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="ol-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center">
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/wpa.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center">
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/wpa.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
-        </div>
-    </div>
-    <div class="row justify-content-center mt-4">
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/mua.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
-        </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/obtis.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
-        </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/dom.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
-        </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center">
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/aig.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
-        </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center">
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/aig.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
     </div>
-    <div class="row justify-content-center mt-4">
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/mua.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+    <div class="row justify-content-center">
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/obtis.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center" >
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/dom.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
-        <div class="col-md-3 mb-4 d-flex justify-content-center">
-            <div class="card" style="border-radius: 10px; background: #CC2E2E; width: 230px; height: 220px; flex-shrink: 0; background-image: url({{ asset('assets/aig.png') }});  background-position: center; background-size: cover; background-repeat: no-repeat; background-size: auto; "></div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="col-md-2 col-sm-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
         </div>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-2 col-sm-2 mb-3  d-flex justify-content-center">            
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="col-md-2 col-sm-2 mb-3  d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="col-md-2 col-sm-2 mb-3  d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="col-md-2 col-sm-2 mb-3  d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="col-md-2 col-sm-2 mb-3  d-flex justify-content-center"></div>
+       
+    </div>
+   
+</div>
+
+<div class = "container d-md-none">
+    <div class="row justify-content-center">
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card"  style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">           
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">            
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center">
+            <div class="card" style="background-image: url({{ asset('assets/fgen.png') }});"></div>
+        </div>
+        <div class="cardView col-4 col-sm-4 col-md-2 mb-3 d-flex justify-content-center"></div>
+       
+    </div>
+   
 </div>
 
 @endsection
