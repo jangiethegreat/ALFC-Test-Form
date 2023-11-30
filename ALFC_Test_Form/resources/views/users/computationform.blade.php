c69cbc6d73d418d067a799149f7e47f0224e429d
jangiethegreat <fernandojohncris@gmail.com>
2023-11-30 10:32:57 +0800

    adding modal on submit button

--- ALFC_Test_Form/resources/views/users/computationform.blade.php
+++ ALFC_Test_Form/resources/views/users/computationform.blade.php
@@ -417,9 +417,30 @@
                         </div>
 
                             <div class="d-flex justify-content-end">
-                                <button type="submit" class="btn btn-danger mt-3">Submit</button>
+                                <button type="submit" class="btn btn-danger mt-3" data-toggle="modal" data-target="#exampleModal">Submit</button>
                             </div>
-
+                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Proceed with Quotation Submission</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body ">
+                                    Please note that all entered information will be subjected to a thorough validation process before final approval is granted. Once you submit the details, they will be forwarded for review and subsequent approval. Kindly ensure the accuracy and completeness of the information provided, as once the submission is confirmed, no further modifications or edits can be made
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-secondary ml-2" data-dismiss="modal"  style="background-color: transparent; border: none; color: red;" >
+                                            Cancel
+                                        </button>
+                                        <button type="button" class="btn btn-danger" style="border-radius: 20px;" >Proceed</button>
+                                        <!-- Add another button here if needed -->
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
 
 
