9736269b9f12a13012ec2acd8dd896aee28ac504
jangiethegreat <fernandojohncris@gmail.com>
2023-12-07 09:58:01 +0800

    test

--- ALFC_Test_Form/resources/views/layouts/app.blade.php
+++ ALFC_Test_Form/resources/views/layouts/app.blade.php
@@ -85,5 +85,14 @@
 
      <!-- DataTables JS -->
      <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+
+     <!-- Include Required Prerequisites -->
+        <script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap/3/css/bootstrap.css" />
+        
+        <!-- Include Date Range Picker -->
+        <script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
 </body>
 </html>
